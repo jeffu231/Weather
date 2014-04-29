93dad602b5525aa8b8696fa1a60ef53846213e05
Jeff Uchitjil <jeffu@kb9kld.org>
2014-04-29 16:30:12 -0500

    Remove unneeded temp folder.

--- tmp/WebContent/js/app/model.viewmodel-config-function.js
+++
@@ -1,9 +0,0 @@
-define(function(){
-	var readingApiUrl = "api";
-	var viewModelConfiguration = function(url, type){
-		this.url = readingApiUrl;
-		this.type = type ? type : "GET";
-	};
-	
-	return viewModelConfiguration;
-});
